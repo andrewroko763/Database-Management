70ff451763c7f5cb50a9ccde1d7cf61ddff3e5de
andrewroko763 <andrew.rokoszak1@marist.edu>
2016-02-09 10:21:37 -0500

    Lab 3 Query 9
    This commit contains queries 7-9.
    
    Query 7 lists all information for orders in January or March.
    
    Query 8 lists all data for orders in February less than us$500.
    
    Query 9 lists all orders from customer who’s cid is C005.

---
+++ Lab 3.9 SQL Queries.sql
@@ -0,0 +1,38 @@
+﻿--select ordnum, totalUSD
+--from orders;
+
+
+--select name, city
+--from agents
+--where name ='Smith';
+
+--select pid, name, priceUSD
+--from products
+--where quantity > 208000;
+
+--select name, city
+--from customers
+--where city ='Dallas';
+
+--select name 
+--from agents
+--where city <> 'New York' and city <> 'Tokyo';
+
+--select *
+--from products
+--where city <> 'Dallas' and city <> 'Duluth'
+--and priceUSD >= 1;
+
+--select *
+--from orders
+--where mon = 'jan' or mon = 'mar';
+
+--select *
+--from orders 
+--where mon = 'feb'
+--and totalUSD < 500;
+
+select *
+from orders
+--where cid = 'C005';
+
